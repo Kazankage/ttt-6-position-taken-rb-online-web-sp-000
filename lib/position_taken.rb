3a845ac3501b6f8f4f03887d14a6c2067bc347c6
Randel Jiao <randeljiao@gmail.com>
2020-07-21 21:23:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board,index)
    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
      taken = false
    else
-     taken = trued
+     taken = true
    end
    taken
   
